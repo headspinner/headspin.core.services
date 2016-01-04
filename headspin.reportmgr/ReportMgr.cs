f7e1a66958e9b4bae51ab03c36c679aa69a2ed90
chanksheadspin <chris@headspinsoftware.com>
2016-01-04 16:41:45 -0500

    Initial solution setup.

---
+++ headspin.reportmgr/ReportMgr.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace headspin.reportmgr
+{
+    public class ReportMgr
+    {
+
+        public static System.IO.StringWriter Download(string name)
+        {
+            System.IO.StringWriter writer = new System.IO.StringWriter();
+
+            return writer;
+        }
+
+        
+
+    }
+
+
+
+}
---
+++ headspin.reportmgr/StoredProcedureReporter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace headspin.reportmgr
+{
+    class StoredProcedureReporter : IReporter
+    {
+        public static System.IO.StringWriter Download()
+        {
+
+        }
+    }
+}
